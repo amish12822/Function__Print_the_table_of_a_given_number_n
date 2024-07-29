1b59de3d1a22ad0c2a53bc4c3de8151b7d6de8a6
amish12822 <amishofficial23@gmail.com>
2024-07-29 14:07:37 +0530

    update to 0 condition

--- print_a_table.java
+++ print_a_table.java
@@ -7,6 +7,12 @@ public class print_a_table {
 
     public static void table(int n){
 
+        if ( n == 0){
+            System.out.println("Can't print 0 table");
+
+            return;
+        }
+
         for (int i=1; i<=10; i++){
 
             System.out.println(n + " * " + i + " = " + n*i);
